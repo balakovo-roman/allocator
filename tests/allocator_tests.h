fc8c7bb44fd13790d7c498299d808f7d99c25f4e
Lebedev Roman <balakovo_roman@mail.ru>
2022-04-16 13:41:05 +0300

    Added initial structure the project

---
+++ tests/allocator_tests.h
@@ -0,0 +1,8 @@
+#ifndef ALLOCATOR_TESTS_ALLOCATOR_TESTS_H_
+#define ALLOCATOR_TESTS_ALLOCATOR_TESTS_H_
+
+#include <gtest/gtest.h>
+
+#include "allocator.h.h"
+
+#endif //ALLOCATOR_TESTS_ALLOCATOR_TESTS_H_
